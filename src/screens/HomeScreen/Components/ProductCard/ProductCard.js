729300fcbd73e6ffed45d2a2c68152bec4c65683
alpersalik <salika@mef.edu.tr>
2022-03-27 14:54:19 +0300

    bug update

--- src/screens/HomeScreen/Components/ProductCard/ProductCard.js
+++ src/screens/HomeScreen/Components/ProductCard/ProductCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 import styles from './ProductCard.style'
 
 export default function ProductCard({ itemData, onPress }) {
-    const isImageAvailable = itemData.avatar.slice(0, 5) === 'https'
+    const isImageAvailable = itemData?.avatar?.slice(0, 5) === 'https'
     return (
         <TouchableOpacity
             style={styles.container}
@@ -16,14 +16,14 @@ export default function ProductCard({ itemData, onPress }) {
                 <Image
                     style={styles.productImage}
                     source={{
-                        uri: isImageAvailable ? itemData.avatar : 'https://via.placeholder.com/150',
+                        uri: isImageAvailable ? itemData?.avatar : 'https://via.placeholder.com/150',
                     }}
                 />
             </View>
             <View style={styles.bottomContainer}>
-                <Text style={styles.productName}>{itemData.name}</Text>
+                <Text style={styles.productName}>{itemData?.name}</Text>
                 <View style={styles.row}>
-                    <Text style={styles.productPrice}>${itemData.price}</Text>
+                    <Text style={styles.productPrice}>${itemData?.price}</Text>
                     <Text style={styles.icon}>✎</Text>
                 </View>
             </View>
